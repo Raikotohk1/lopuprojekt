e54e552b6918dcb71be76f0f5cbab1b60be55f32
Raiko <raiko.tohk@ametikool.ee>
2024-04-14 21:04:59 +0300

    models

--- lopuprojekt/database/migrations/2024_04_14_151510_performance.php
+++ lopuprojekt/database/migrations/2024_04_14_151510_performance.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('your_uritus', function (Blueprint $table) {
+        Schema::create('performance', function (Blueprint $table) {
             $table->id();
             $table->date('date');
             $table->string('location');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('your_uritus');
+        Schema::dropIfExists('performance');
     }
 };
